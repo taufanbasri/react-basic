7f6a9db560b9fcc89c80876fa96e09c24e96fc5b
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-07-20 19:32:59 +0700

    Clean

--- src/App.js
+++ src/App.js
@@ -1,36 +1,14 @@
-import Greet from "./components/Greet";
-import Welcome from "./components/Welcome";
-import Message from "./components/Message";
+import React, { Component } from 'react';
+import './App.css'
 
-import "./App.css";
-import Counter from "./components/Counter";
-import FunctionClick from "./components/FunctionClick";
-import ClassClick from "./components/ClassClick";
-import EventBind from "./components/EventBind";
-import ParentComponent from "./components/ParentComponent";
-import UserGreeting from "./components/UserGreeting";
-import NameList from "./components/NameList";
-
-function App() {
-  return (
-    <div className="App">
-      <NameList />
-      {/* <UserGreeting /> */}
-      {/* <ParentComponent /> */}
-      {/* <EventBind /> */}
-      {/* <FunctionClick /> */}
-      {/* <ClassClick /> */}
-      {/* <Counter /> */}
-      {/* <Message /> */}
-      {/* <Greet name="Taufan" heroName="Tamvan">
-        <p>This is children</p>
-      </Greet> */}
-      <hr />
-      {/* <Welcome name="Taufan" heroName="Tamvan">
-        <p>This is children</p>
-      </Welcome> */}
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        
+      </div>
+    );
+  }
 }
 
 export default App;
