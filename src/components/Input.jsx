9b76acec8d3f196b491a7b108986fb0d0ab4f758
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-07-22 05:37:12 +0700

    Refs with class component, its cannot attach to functional components

---
+++ src/components/Input.jsx
@@ -0,0 +1,23 @@
+import React, { Component, createRef } from "react";
+
+class Input extends Component {
+  constructor(props) {
+    super(props);
+
+    this.inputRef = React.createRef();
+  }
+
+  focusInput() {
+    this.inputRef.current.focus();
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="text" ref={this.inputRef} />
+      </div>
+    );
+  }
+}
+
+export default Input;
