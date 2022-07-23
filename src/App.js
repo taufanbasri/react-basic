6bbece5d99055d156d1665ce5c97358aed98b4c4
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-07-23 13:15:02 +0700

    HTTP Post Request

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
+import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <PostForm />
         <PostList />
       </div>
     );
